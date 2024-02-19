e98b308ddff97e36b6f7545df463e75bbcb3f23e
bayardo <bayardojesus@gmail.com>
2024-02-19 18:56:44 -0500

    as1asas

--- src/service/apiservice.js
+++ src/service/apiservice.js
@@ -5,70 +5,72 @@ const ttl = 180000;
 const users = new Keyv();
  var datamessages = require("../controller/messages");
 
- const   EnviarMensajeWhastpapp=function(objetoMensaje){
+ async function  EnviarMensajeWhastpapp(  objetoMensaje){
+
+
+
+    try {
+        currentUser = await users.get(number);
+        console.log("Current user:", currentUser);
+      } catch (error) {
+        console.error("Error retrieving user:", error);
+      }
+
+
    
+    console.log(objetoMensaje);
+    var messages = objetoMensaje[0];
 
-    objetoMensaje.onMessage(async message => {
-
-
-        let currentUser =  await users.get(number);
-        var texto = messages["text"]["body"];
-        var number = messages["from"];
-
-console.log("-----");
-console.log(currentUser);
-        if (!currentUser) {
-            console.log("NO existe")
-             users.set(message.from, ["root"], ttl);
-         
-    
-             var data = JSON.stringify({
-                "messaging_product": "whatsapp",
-                "recipient_type": "individual",
-                "to": number,
-                "type": "text",
-                "text": {
-                    "preview_url": false,
-                    "body": "🚀 Hola, visita mi web anderson-bastidas.com para mas información.\n \n📌Por favor, ingresa un numero #️⃣ para recibir información.\n \n1️⃣. Información del Curso. ❔\n2️⃣. Ubicación del local. 📍\n3️⃣. Enviar temario en pdf. 📄\n4️⃣. Audio explicando curso. 🎧\n5️⃣. Video de Introducción. ⏯️\n6️⃣. Hablar con AnderCode. 🙋‍♂️\n7️⃣. Horario de Atención. 🕜"
-                }
-            });
-    
-        }else{
-            console.log("existe")
-        }
+    var texto = messages["text"]["body"];
+    var number = messages["from"];
 
 
+    texto = texto.toLowerCase();
+    let currentUser = await  users.get(number);
+ console.log("----");
+ console.log(currentUser);
 
+    if (!currentUser) {
+         users.set(message.from, ["root"], ttl);
+     
 
-
-        const options = {
-            host : "graph.facebook.com",
-            path : "/v18.0/120525987808147/messages",
-            method : "POST",
-            body : data,
-            headers : {
-                "Content-Type" : "application/json",
-                Authorization :"Bearer EAASsECeG2AkBOxYZCq8ogj7dFoE7ZBS4A0ZCUPa6RHj90JmqbbFPKOZAs8BQOhtExUZBMMt4wfOJUL1RQFFZCerTJiSZBST9cZBjSJrrdkPQZAEzeluf3VAxNzSTT7MbZCRbS31XKOOZCTE4rZAB1zMNGP32d3wamUV4h3BJXdDKZC0EU9KEm2mA17WkPNY4FpvvY7kZC9SNC5mArJElRTxvtSZAQUZD"
+         var data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": number,
+            "type": "text",
+            "text": {
+                "preview_url": false,
+                "body": "🚀 Hola, visita mi web anderson-bastidas.com para mas información.\n \n📌Por favor, ingresa un numero #️⃣ para recibir información.\n \n1️⃣. Información del Curso. ❔\n2️⃣. Ubicación del local. 📍\n3️⃣. Enviar temario en pdf. 📄\n4️⃣. Audio explicando curso. 🎧\n5️⃣. Video de Introducción. ⏯️\n6️⃣. Hablar con AnderCode. 🙋‍♂️\n7️⃣. Horario de Atención. 🕜"
             }
-        };
-    
-        const req = https.request(options,res => {
-            res.on("data",d=>{
-                process.stdout.write(d);
-            });
         });
 
-        req.write(data);
-        req.end();
+    }else{
+        console.log("existe")
+    }
+
+
+
+
+    const options = {
+        host : "graph.facebook.com",
+        path : "/v18.0/120525987808147/messages",
+        method : "POST",
+        body : data,
+        headers : {
+            "Content-Type" : "application/json",
+            Authorization :"Bearer EAASsECeG2AkBOxYZCq8ogj7dFoE7ZBS4A0ZCUPa6RHj90JmqbbFPKOZAs8BQOhtExUZBMMt4wfOJUL1RQFFZCerTJiSZBST9cZBjSJrrdkPQZAEzeluf3VAxNzSTT7MbZCRbS31XKOOZCTE4rZAB1zMNGP32d3wamUV4h3BJXdDKZC0EU9KEm2mA17WkPNY4FpvvY7kZC9SNC5mArJElRTxvtSZAQUZD"
+        }
+    };
+
+    const req = https.request(options,res => {
+        res.on("data",d=>{
+            process.stdout.write(d);
+        });
     });
 
-   
-
-
-
-   
-
-   
+    req.write(data);
+    req.end();
 }
 
 module.exports = {
